20d5245bf30c5d31a31bb34c4b5c52459a1df9e1
emdadripon <eriponctg@gmail.com>
2020-07-27 20:15:08 -0400

    test added

--- src/test/java/pagestests/HomePageTests.java
+++ src/test/java/pagestests/HomePageTests.java
@@ -1,6 +1,10 @@
 package pagestests;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -10,12 +14,29 @@ import pages.Homepage;
 
 public class HomePageTests extends CommonApi {
 	
-	@Test
+	@Test(enabled = false, priority = 0, description = "Verify KP page")
 	public void kp() {
 		
 		Homepage homepage = PageFactory.initElements(driver, Homepage.class);
 		homepage.whykpPage();
 		Assert.assertTrue(driver.findElement(By.xpath("/html/body/main/div[1]/div[2]/div/div/div/div[1]/h1/span")).isDisplayed());
+	}
+	
+	@Test(enabled = false)
+	public void images() {
+		
+		List<WebElement> images = driver.findElements(By.tagName("img"));
+		for(WebElement allimages:images) {
+			System.out.println(allimages.getAttribute("src"));
+		}
+	}
+	
+	@Test
+	public void scroll() {
+		WebElement ele = driver.findElement(By.linkText("Get Care"));
+		JavascriptExecutor js = ((JavascriptExecutor)driver);
+		js.executeScript("arguments[0].scrollIntoView(true);", ele );
+		ele.click();
 	}
 
 }
